92927dee001b7e4f043d455cdaf9843e9c4fed80
jongmin park <showppark12@naver.com>
2020-05-25 21:01:10 +0900

    setting

---
+++ beerboard/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BeerboardConfig(AppConfig):
+    name = 'beerboard'
---
+++ studyboard/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StudyboardConfig(AppConfig):
+    name = 'studyboard'
