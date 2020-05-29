75eaa5676cf0ba0453f5e0a5bc307ed26fbd465e
dadahee <heeouo@kookmin.ac.kr>
2020-05-29 21:11:15 +0900

    수정중

--- foodboard/admin.py
+++ foodboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import FoodBoard
+from .models import FoodBoard, FoodComment
 
 # Register your models here.
 admin.site.register(FoodBoard)
+admin.site.register(FoodComment)
