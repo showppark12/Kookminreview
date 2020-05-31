7c02f3498eb23251745bcb30656d4b217d41c8f3
dadahee <heeouo@kookmin.ac.kr>
2020-05-31 20:12:20 +0900

    덧글 수정, 삭제

--- foodboard/views.py
+++ foodboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import FoodBoard
+from .models import FoodBoard, FoodComment
 from .form import FoodForm
 from django.utils import timezone
 from django.contrib.auth.models import User 
@@ -12,7 +12,8 @@ def FBoardHome(request):
 
 def FBoardDetail(request, review_id):
     review = get_object_or_404(FoodBoard, pk=review_id)
-    return render(request, 'FBoardDetail.html', { 'review': review })
+    comments= review.comments.all()
+    return render(request, 'FBoardDetail.html', { 'review': review, 'comments': comments })
 
 
 def FBoardNew(request):
@@ -55,21 +56,17 @@ def FBoardDelete(request, review_id):
 
 
 def new_comment(request, review_id):
-    if request.method == 'GET':
-        form = FoodForm()
-        return render(request, 'FBoardNew.html', { 'form': form })
-    else:
-        form = FoodForm(request.POST, request.FILES)
-        # 입력 데이터 유효성 검사 해주기
-        if form.is_valid():
-            content = form.save(commit=False) #데이터 임시 저장(이후 살 붙이고 제대로 저장)
-            content.pub_date = timezone.now()
-            content.writer = request.user
-            content.save()
-            return redirect('food_home')
-
-def edit_comment(request, comment_id):
-    pass
+    if request.method == 'POST':
+        new_comment = FoodComment()
+        new_comment.writer = get_object_or_404(User, username=request.user)
+        new_comment.board = get_object_or_404(FoodBoard, pk = review_id)
+        new_comment.pub_date = timezone.datetime.now()
+        new_comment.text = request.POST['text']
+        new_comment.save()
+    return redirect('FBoardDetail', review_id)
 
 def delete_comment(request, comment_id):
-    pass
+    del_comment = get_object_or_404(FoodComment, pk = comment_id)
+    review_id = del_comment.board.id
+    del_comment.delete()
+    return redirect('FBoardDetail', review_id)
